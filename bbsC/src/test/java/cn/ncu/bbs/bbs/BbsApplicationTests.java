2c6a6b4d8afb7a1ee7ece8e5ddaf818a8d9b2ff7
cuishimiao <33611640+cuishimiao@users.noreply.github.com>
2019-12-10 11:15:49 +0800

    version1.0
    this version has a Main interface and  Login function 。

---
+++ bbsC/src/test/java/cn/ncu/bbs/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ncu.bbs.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
