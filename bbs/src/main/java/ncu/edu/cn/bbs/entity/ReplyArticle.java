b7dce9280f72082a25d361d44df741fd350c2275
orivalwu <1093077690@qq.com>
2019-12-16 15:17:10 +0800

    实体类子字段命名改为驼峰

--- bbs/src/main/java/ncu/edu/cn/bbs/entity/ReplyArticle.java
+++ bbs/src/main/java/ncu/edu/cn/bbs/entity/ReplyArticle.java
@@ -7,10 +7,10 @@ import java.sql.Timestamp;
 @Alias("replyArticle")
 public class ReplyArticle {
     private int id;
-    private int article_id;
-    private int responder_id;
+    private int articleId;
+    private int responderId;
     private String context;
-    private Timestamp reply_time;
+    private Timestamp replyTime;
 
     public int getId() {
         return id;
@@ -20,20 +20,20 @@ public class ReplyArticle {
         this.id = id;
     }
 
-    public int getArticle_id() {
-        return article_id;
+    public int getArticleId() {
+        return articleId;
     }
 
-    public void setArticle_id(int article_id) {
-        this.article_id = article_id;
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
     }
 
-    public int getResponder_id() {
-        return responder_id;
+    public int getResponderId() {
+        return responderId;
     }
 
-    public void setResponder_id(int responder_id) {
-        this.responder_id = responder_id;
+    public void setResponderId(int responderId) {
+        this.responderId = responderId;
     }
 
     public String getContext() {
@@ -44,22 +44,22 @@ public class ReplyArticle {
         this.context = context;
     }
 
-    public Timestamp getReply_time() {
-        return reply_time;
+    public Timestamp getReplyTime() {
+        return replyTime;
     }
 
-    public void setReply_time(Timestamp reply_time) {
-        this.reply_time = reply_time;
+    public void setReplyTime(Timestamp replyTime) {
+        this.replyTime = replyTime;
     }
 
     @Override
     public String toString() {
         return "ReplyArticle{" +
                 "id=" + id +
-                ", article_id=" + article_id +
-                ", responder_id=" + responder_id +
+                ", articleId=" + articleId +
+                ", responderId=" + responderId +
                 ", context='" + context + '\'' +
-                ", reply_time=" + reply_time +
+                ", replyTime=" + replyTime +
                 '}';
     }
 }
