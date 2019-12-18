512801b25086eeffe2208b15b4d650041024c326
lu-teng <1738280546@qq.com>
2019-12-18 21:46:23 +0800

    编辑文章回复管理

--- bbs/src/main/java/ncu/edu/cn/bbs/controller/ManageReply.java
+++ bbs/src/main/java/ncu/edu/cn/bbs/controller/ManageReply.java
@@ -1,31 +1,36 @@
 package ncu.edu.cn.bbs.controller;
 
+import ncu.edu.cn.bbs.entity.Article;
+import ncu.edu.cn.bbs.entity.QuestionReply;
 import ncu.edu.cn.bbs.entity.ReplyArticle;
 import ncu.edu.cn.bbs.service.ReplyArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @Author:wzh
+ * @Description:
+ * @Date:Createed in 2019/12/10 16:01
+ **/
 @Controller
 public class ManageReply {
 
     @Autowired
     private ReplyArticleService articleService;
 
-    @RequestMapping("/Replys")
-    public String findAll(Model m){
-        List<ReplyArticle> all = articleService.findAll();
-        m.addAttribute("msg",all);
-        return "admin/managecomment";
-    }
+//    @RequestMapping("/Replys")
+//    public String findAll(Model m){
+//        List<ReplyArticle> all = articleService.findAll();
+//        m.addAttribute("msg",all);
+//        return "admin/managecomment";
+//    }
 
     @RequestMapping("/deleteRep/{id}")
     public String delete(@PathVariable int id){
@@ -50,7 +55,92 @@ public class ManageReply {
         List<ReplyArticle> replys = articleService.findAllReply(i);
         m.addAttribute("searchname",searchname);
         m.addAttribute("msg",replys);
+
+        m.addAttribute("find",1);
         return "admin/managecomment";
     }
+
+
+    @RequestMapping("/modifym/{id}")
+    public String modifyu(@PathVariable int id, Model m){
+        m.addAttribute("msg",articleService.getReply(id));
+        return "admin/managemodifycom";
+    }
+
+    @PostMapping("/modifycom")
+    public String modifyuser(ReplyArticle reply){
+        articleService.modifyReply(reply);
+        return "redirect:/Replys";
+    }
+
+
+    @RequestMapping("/Replys")
+    public String sspage(@RequestParam(value = "page",defaultValue = "1")String page,@RequestParam(value = "tag",defaultValue = "1")String tag,Model m){
+        int size=2; //一页的大小
+        int j=1;    //初始值
+        System.out.println("查询评论***************");
+        if(tag.equals("1")) //当前页面
+        {
+            j=Integer.parseInt(page);
+        }
+        else if(tag.equals("0"))    //上一页
+        {
+            j=Integer.parseInt(page)-1;
+        }
+        else if(tag.equals("2"))    //下一页
+        {
+            j=Integer.parseInt(page)+1;
+        }
+        j--;        //选择的页数-1
+
+        if(j<0)     //不能小于0
+            j=0;
+
+        List<ReplyArticle> all = articleService.findAll();  //所有内容
+
+        int pagenum=all.size()/size;
+        if(all.size()%size>0)
+        {
+            pagenum++;      //页面数
+        }
+        if(j>=pagenum)
+        {
+            j=pagenum-1;    //j允许的最大页数
+        }
+
+        List<ReplyArticle> apage = new ArrayList<ReplyArticle>(); //每页要显示的内容
+        int i=0;
+        for(ReplyArticle h:all)
+        {
+            i++;
+            if(i>j*size&&i<=(j+1)*size)
+            {
+                apage.add(h);   //选择要显示的内容
+                System.out.println(i);
+            }
+        }
+
+
+        System.out.println("页数："+pagenum);
+        m.addAttribute("msg",apage);
+
+        List<Integer> num = new ArrayList<Integer>();
+        for(int k=0;k<pagenum;k++)
+        {
+            num.add((Integer)(k+1));
+            System.out.println(k);
+        }
+        m.addAttribute("num",num);//页面数
+        System.out.println(num.get(0));
+
+        m.addAttribute("currentpage",j+1);  //当前页面
+        System.out.println(j+1);
+        System.out.println("当前"+(j+1));
+        //如果没传参数，则type为managesearch，传了参数则为则type为managemodify
+        return "admin/managecomment";
+    }
+
+
+
 
 }
