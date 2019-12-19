1f1aace1cd6a16495383b9e17c5c9954c2b0b16b
cuishimiao <33611640+cuishimiao@users.noreply.github.com>
2019-12-19 08:15:48 +0800

    delete two inteface

--- bbs/src/main/java/ncu/edu/cn/bbs/controller/CategoryController.java
+++ bbs/src/main/java/ncu/edu/cn/bbs/controller/CategoryController.java
@@ -4,6 +4,7 @@ import ncu.edu.cn.bbs.entity.Category;
 import ncu.edu.cn.bbs.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,6 +18,21 @@ public class CategoryController {
     @ResponseBody
     public List<Category>findCategory(){
         return categoryService.findAllCategory();
+    }
+
+    //标签管理
+    @RequestMapping("/tags")
+    public String manageTags(Model model)
+    {
+        List<Category> tags = categoryService.findAllCategory();
+        model.addAttribute("tags", tags);
+        return "tags";
+    }
+
+    @RequestMapping("/admin/tags/input")
+    public String addTags()
+    {
+        return "/admin/tagsInput";
     }
 
 }
